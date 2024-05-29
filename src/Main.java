733949fef347bcd212aa287375e13bc34534ae2e
Ilyas <fortwink88@gmail.com>
2024-05-30 02:12:17 +0500

    Testing. Source code from Lecture's code.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+        WeightedGraph<String> weightedGraph = new WeightedGraph<>(false);
+        fillWithWeights(weightedGraph);
+
+        System.out.println("Dijkstra:");
+        Search<String> djk = new DijkstraSearch<> (weightedGraph, "Almaty");
+        outputPath(djk, "Kyzylorda");
+
+        System.out.println("--------------------------------");
+
+        System.out.println("BFS:");
+        Search<String> bfs = new BreadthFirstSearch<>(weightedGraph, "Almaty");
+        outputPath(bfs, "Kyzylorda");
+    }
+
+    public static void fillWithWeights(WeightedGraph<String> graph) {
+        graph.addEdge("Almaty", "Astana", 2.1);
+        graph.addEdge("Shymkent", "Atyrau", 7.8);
+        graph.addEdge("Atyrau", "Astana", 7.1);
+        graph.addEdge("Almaty", "Shymkent", 7.2);
+        graph.addEdge("Shymkent", "Astana", 3.9);
+        graph.addEdge("Astana", "Kostanay", 3.5);
+        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
+    }
+
+    public static void outputPath(Search<String> search, String key) {
+        for (String v : search.pathTo(key)) {
+            System.out.print(v + " -> ");
+        }
+        System.out.println();
     }
 }
